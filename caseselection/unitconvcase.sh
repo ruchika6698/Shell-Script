94f1fe9444a84eca8ddb6dfbe27c11ecf9a8bc64
nilamkurhade <nilamkurhade87@gmail.com>
2020-03-16 19:12:47 +0530

    Unit conversion using case

---
+++ caseselection/unitconvcase.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+
+
+read -p "Enter a choice: " choice
+
+case  $choice in 
+			1)
+				read -p "Enter a Number: " Number
+				Feet=$(echo "scale=2; $Number*12" | bc)
+				printf " $Number=$Feet inch"'\n'
+			;;
+
+			2)
+				read -p "Enter a Number: " Number
+				Feet=$(echo "scale=2; $Number*0.3048" | bc)
+				printf " $Number=$Feet meter"'\n'
+			;;
+			
+			3)
+				read -p "Enter a Number: " Number
+				Inch=$(echo "scale=2; $Number/12" | bc)
+				printf " $Number=$Inch ft"'\n'
+			;;
+
+ 	 		4)
+				read -p "Enter a Number: " Number
+				Meter=$(echo "scale=2; $Number/0.3048" | bc)
+				printf " $Number=$Meter ft"'\n'
+			;;				
+			*)
+				echo "Wrong choice"
+			;;
+esac
