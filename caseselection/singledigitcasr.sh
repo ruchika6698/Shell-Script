4de517ea44f27bfdfe759627c636136df4cf0f46
nilamkurhade <nilamkurhade87@gmail.com>
2020-03-17 10:40:50 +0530

    Display single in word using case

--- caseselection/singledigitcasr.sh
+++ caseselection/singledigitcasr.sh
@@ -4,40 +4,34 @@ read -p "Enter a Number:" N
 
 
 case  $N in 
-   		1)
-				echo "one"
-			;;
-
-			2)
-				echo "Two"
-			;;
-
-
-			3)
-				echo "Three"
-			;;
-
- 	 		4)
-				echo "Four"
-			;;
-    		5)
-				echo "Five"
-			;;
-
-    		6)
-				echo "Six"
-			;;
-
-    		7)
-				echo "Seven"
-			;;
-			8)
-				echo "Eight"
-			;;
-			9)
-				echo "Nine"
-			;;
-			*)
-				echo "Invalid Input"
-			;;
+	1)
+		echo "one"
+	;;
+	2)
+		echo "Two"
+	;;
+	3)
+		echo "Three"
+	;;
+	4)
+		echo "Four"
+	;;
+	5)
+		echo "Five"
+	;;
+	6)
+		echo "Six"
+	;;
+	7)
+		echo "Seven"
+	;;
+	8)
+		echo "Eight"
+	;;
+	9)
+		echo "Nine"
+	;;
+	*)
+		echo "Invalid Input"
+	;;
 esac
