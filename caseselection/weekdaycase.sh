fcb51ddf3d48bdf7f647c32a3949708d726ff6a4
nilamkurhade <nilamkurhade87@gmail.com>
2020-03-17 10:42:55 +0530

    weekdays in word using case

--- caseselection/weekdaycase.sh
+++ caseselection/weekdaycase.sh
@@ -3,33 +3,28 @@
 read -p "Enter a Number: " N
 
 case  $N in 
-   		1)
-				echo "Day is sunday"
-			;;
-
-			2)
-				echo "Day is monday"
-			;;
-			3)
-				echo "Day is Tuesday"
-			;;
-
- 	 		4)
-				echo "Day is wedenesday"
-			;;
-    		5)
-				echo "Day is Thursday"
-			;;
-
-    		6)
-				echo "Day is Friday"
-			;;
-
-    		7)
-				echo "Day is saturday"
-			;;
-			*)
-				echo "Day Doesn't Exist"
-			;;
-
+	1)
+		echo "Day is sunday"
+	;;
+	2)
+		echo "Day is monday"
+	;;
+	3)
+		echo "Day is Tuesday"
+	;;
+	4)
+		echo "Day is wedenesday"
+	;;
+   5)
+		echo "Day is Thursday"
+	;;
+	6)
+		echo "Day is Friday"
+	;;
+	7)
+		echo "Day is saturday"
+	;;
+	*)
+		echo "Day Doesn't Exist"
+	;;
 esac
