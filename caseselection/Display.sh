591cf93e4e315e4121f83f96cfd8a22daf50e59d
nilamkurhade <nilamkurhade87@gmail.com>
2020-03-17 09:58:39 +0530

    Display digit in word using case

--- caseselection/Display.sh
+++ caseselection/Display.sh
@@ -3,27 +3,22 @@
 read -p "Enter a Number: " N
 
 case  $N in 
-   		1)
-				echo "Unit"
-			;;
-
-			10)
-				echo "Ten"
-			;;
-
-
-			100)
-				echo "Hundred"
-			;;
-
- 	 		1000)
-				echo "Thousand"
-			;;
-    		10000)
-				echo "Ten Thousand"
-			;;
-
-			*)
-				echo "Invalid Input"
-			;;
+	1)
+		echo "Unit"
+	;;
+	10)
+		echo "Ten"
+	;;
+	100)
+		echo "Hundred"
+	;;
+	1000)
+		echo "Thousand"
+	;;
+	10000)
+		echo "Ten Thousand"
+	;;
+	*)
+	echo "Invalid Input"
+	;;
 esac
